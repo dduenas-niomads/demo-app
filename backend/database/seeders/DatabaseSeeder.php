db5722aa87c995c5a9f28177b6522f7bb3890185
Christian <christiancasabuena@gmail.com>
2025-07-10 13:57:45 +0200

    modificado DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ConceptSeeder::class,
             CountrySeeder::class,
+            CitySeeder::class,
         ]);
     }
 }
